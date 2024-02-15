9c6048223f978d83aeabe99e2347b3d317ad3f99
Kishan.G.A <itsxzanonymous@gmail.com>
2024-02-15 20:30:09 +0530

    Initial commit

---
+++ instalar.sh
@@ -0,0 +1,10 @@
+clear
+apt-get update && apt-get upgrade
+pkg install nodejs
+pkg install toilet
+pkg install mpv 
+clear
+echo "\033[1;32m obrigado por instala esta script ❤️😍"
+
+sh venom.sh
+
---
+++ venom.sh
@@ -0,0 +1,5 @@
+# Encrypted by Venom
+
+z="
+";Dz=' '\''\n';Gz='et -';Lz='l Ve';Kz='meta';Pz=' ven';Qz='om';Az='clea';Mz='nomB';Ez='\n'\''';Jz=' -F ';Oz='node';Fz='toil';Hz='F bo';Cz='echo';Bz='r';Nz='ot';Iz='rder';
+eval "$Az$Bz$z$Cz$Dz$Ez$z$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$z$Oz$Pz$Qz"
